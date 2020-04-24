6c5bc606d4062e5bc547893325f1e4b2150e5b06
BeeWang015 <bkwang015@outlook.com>
2020-04-23 17:43:30 -0700

    Edited loginRoute to correctly use login post method from the controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,7 @@ const loginModel = require('../models/logInData');
 
 // TODO: @Daniel or @Benson define login controller functions here
 
-exports.postLogIn = (req, res) => {
+exports.postLogin = (req, res) => {
     res.render('', {
         title: 'Login Page',
         data: req.body
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const logInController = require("../controllers/loginController");
 
-router.get('/', logInController.post);
+router.get('/', logInController.postLogin);
 
 module.exports = router;
