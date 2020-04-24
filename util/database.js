bc3c0f988817b7e3eb4f8b6f329b09e10d443fbc
Daniel Chu <n4jn4d@gmail.com>
2020-04-24 12:04:00 -0700

    added remotemysql info into database.js

--- util/database.js
+++ util/database.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
-// TODO: @Daniel - Fill up user, database, password via remotemysql.com
 const pool = mysql.createPool({
   host: 'remotemysql.com',
-  user: '',
-  database: '',
-  password: '',
+  user: 'HHTh4BmIT4',
+  database: 'HHTh4BmIT4',
+  password: 'zOl8IYGf1s',
 });
 
 module.exports = pool.promise();
