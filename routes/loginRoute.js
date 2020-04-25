9fb6c3fd79c583fe2ee729d69c291e9bd380bf14
Kevin Vizconde <kcarlv@gmail.com>
2020-04-25 00:56:02 -0700

    updated style.css, and index.css

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { indexJSCSS: true });
+  res.render('index', {
+    title: 'Login Page',
+    indexJSCSS: true,
+  });
 });
 
 module.exports = router;
