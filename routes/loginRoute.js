2cbcaa19d783f0968afff1fac7a26a264fc48c97
Daniel Chu <n4jn4d@gmail.com>
2020-04-27 15:13:00 -0700

    merged benson changes to loginRoute and index

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,23 +1,11 @@
 const express = require('express');
-<<<<<<< HEAD
-const loginController = require("../controllers/loginController");
-=======
->>>>>>> 43116cd0f7ba29f948f892da3841ef16e65718b7
 const router = express.Router();
 const loginController = require("../controllers/loginController");
 
 // Test route that runs getAllPT from the log in controller.
 router.get('/', loginController.getAllPT);
 
-<<<<<<< HEAD
-// Test route that runs getAllPT from the log in controller.
-router.get('/', loginController.getAllPT);
-
-// Route that runs log in authentication.
-router.post('/login', loginController.logIn);
-=======
 // Route that runs log in authentication.
 router.post('/homepage', loginController.postLogIn);
->>>>>>> 43116cd0f7ba29f948f892da3841ef16e65718b7
 
 module.exports = router;
