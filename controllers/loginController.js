4ce8cdbf1faa10952efe180e7981d003924b2eb9
Daniel Chu <n4jn4d@gmail.com>
2020-04-26 23:04:54 -0700

    Daniel (#22)
    * daniel test push
    
    * added remotemysql info into database.js
    
    * added loggedIn.hbs for log in testing
    
    * added test and log in authentication routes to loginRoute.js
    
    * added test and log in functions to loginController.js
    
    * added getAllPTs and logIn functions to loginData.js
    
    * updated log in form action and input name
    
    * changed input type from email to text for physio ID log in

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,36 @@
 const loginModel = require('../models/loginData');
 
-// TODO: @Daniel or @Benson define login controller functions here
-
-exports.postLogin = (req, res) => {
-  res.render('', {
-    title: 'Login Page',
-    data: req.body,
+// Test function that grabs all physiotherapist accounts from the database.
+exports.getAllPT = (req, res, next) => {
+  let pts = loginModel.getAllPTs();
+  pts.then( ([rows, fieldData]) => {
+    res.render('index', {title : "testData",
+                         testData : rows,
+                         indexJSCSS : true
+                        });
   });
-};
+}
+
+// Basic log in authentication function. (Needs some clean up)
+exports.logIn = (req, res) => {
+  let user = req.body.username;
+  let pword = req.body.password;
+
+  if (user && pword) {
+    let attempt = loginModel.logIn(user, pword);
+      attempt.then( ([data, metadata]) => {
+        console.log(typeof data[0]);
+        if ((typeof data[0]) == "object") {
+          res.render("loggedIn", {title : "HOME", indexJSCSS : true, userInfo : data[0]})
+        } else {
+          res.send("Incorrect username or password.");
+        }
+      }).catch( ([data, metadata]) => {
+          res.send("catch: " + data);
+      });
+  } else {
+    res.send("Username or Password cannot be left empty.");
+    res.end();
+  }
+}
+
