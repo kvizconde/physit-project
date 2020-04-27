f6297e931253107f100f25977e8bdb00249116fa
Daniel Chu <n4jn4d@gmail.com>
2020-04-26 22:39:59 -0700

    added test and log in authentication routes to loginRoute.js

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
+const loginController = require("../controllers/loginController");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Login Page',
-    indexJSCSS: true,
-  });
-});
+// Test route that runs getAllPT from the log in controller.
+router.get('/', loginController.getAllPT);
+
+// Route that runs log in authentication.
+router.post('/login', loginController.logIn);
 
 module.exports = router;
