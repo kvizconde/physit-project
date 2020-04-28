2eb097a28905fc0616b29f0374f4e4488d8dde9e
Kevin Vizconde <kcarlv@gmail.com>
2020-04-27 23:58:57 -0700

    Ringo (#29)
    * initial push test
    
    From ringo branch
    
    * index hbs and css updated
    
    minor changes to loginRoute and main.hbs to callback CSS file
    
    * fix conflicts
    
    Co-authored-by: Ringo Suen <43553127+ringosuen@users.noreply.github.com>

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const loginController = require("../controllers/loginController");
 
-// const loginController = require("../controllers/loginController");
 
 // // Test route that runs getAllPT from the log in controller.
 // router.get('/', loginController.getAllPT);
 
-// // Route that runs log in authentication.
+// Route that runs log in authentication.
 // router.post('/homepage', loginController.postLogIn);
 
 // Route that runs log in authentication.
--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
 // remote database
-const pool = mysql.createPool({
-  host: 'remotemysql.com',
-  user: 'HHTh4BmIT4',
-  database: 'HHTh4BmIT4',
-  password: 'zOl8IYGf1s',
-});
+// const pool = mysql.createPool({
+//   host: 'remotemysql.com',
+//   user: 'HHTh4BmIT4',
+//   database: 'HHTh4BmIT4',
+//   password: 'zOl8IYGf1s',
+// });
 
 //local database
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   database: 'comp4944-project',
-//   password: 'COMP-4711',
-// });
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'comp4944-project',
+  password: 'password',
+});
 
 module.exports = pool.promise();
