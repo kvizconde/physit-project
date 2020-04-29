53b192eb50b54ed644c82807017ebe43e68d9489
Kevin Vizconde <kcarlv@gmail.com>
2020-04-29 14:18:53 -0700

    added a wrong password test for index.hbs/css

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,43 +1,51 @@
 const loginModel = require('../models/loginData');
 
-
 exports.getLogin = (req, res, next) => {
-    res.render('index', {
-        title: 'Login Page',
-        indexJSCSS: true,
-    });
-}
+  res.render('index', {
+    title: 'Login Page',
+    indexJSCSS: true,
+  });
+};
 
 // Test function that grabs all physiotherapist accounts from the database.
 exports.getAllPT = (req, res, next) => {
-    const pts = loginModel.getAllPTs();
+  const pts = loginModel.getAllPTs();
 
-    pts.then( ([rows, fieldData]) => {
-      res.render('index', {title: 'testData', 
-                           testData: rows, 
-                           indexJSCSS: true});
-    });
+  pts.then(([
+    rows,
+    fieldData
+  ]) => {
+    res.render('index', { title: 'testData', testData: rows, indexJSCSS: true });
+  });
 };
 
 // Basic log in authentication function. (Needs some clean up)
 exports.postLogIn = (req, res) => {
-    const email = req.body.physioID;
-    const pword = req.body.password;
+  const email = req.body.physioID;
+  const pword = req.body.password;
 
-    const attempt = loginModel.logIn(email, pword);
+  const attempt = loginModel.logIn(email, pword);
 
-    attempt.then( ([data, metadata]) => {
+  attempt
+    .then(([
+      data,
+      metadata
+    ]) => {
       console.log(data[0]);
       console.log(typeof data[0]);
       if (typeof data[0] === 'object') {
-        res.render('patientList', {title: 'HOME', 
-                                  patientListJSCSS: true, 
-                                  indexJSCSS: false, 
-                                  physiotherapist: data[0]});
+        res.render('patientList', {
+          title: 'HOME',
+          patientListJSCSS: true,
+          indexJSCSS: false,
+          physiotherapist: data[0],
+        });
       } else {
-        res.send('Username and password combination is invalid!');
+        // TODO: @Benson or @Daniel please figure out a way to stop directing it to /calendar if password is invalid
+        res.render('index', { title: 'Login Page', indexJSCSS: true, passwordFailed: true });
       }
-    }).catch( (data) => {
-      res.send("catch: " + data);
+    })
+    .catch(data => {
+      res.send(`catch: ${  data}`);
     });
 };
