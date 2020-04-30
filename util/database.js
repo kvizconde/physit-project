85d405c5d7e61694321ccd5daa64f7f3c278408b
Ringo Suen <43553127+ringosuen@users.noreply.github.com>
2020-04-30 12:04:27 -0700

    Ringo (#40)
    * initial push test
    
    From ringo branch
    
    * index hbs and css updated
    
    minor changes to loginRoute and main.hbs to callback CSS file
    
    * fix conflicts
    
    * fix conflict in database.js

--- util/database.js
+++ util/database.js
@@ -2,18 +2,18 @@ const mysql = require('mysql2');
 
 // remote database
 const poolRemote = mysql.createPool({
-  host: 'remotemysql.com',
-  user: 'HHTh4BmIT4',
-  database: 'HHTh4BmIT4',
-  password: 'zOl8IYGf1s',
+    host: 'remotemysql.com',
+    user: 'HHTh4BmIT4',
+    database: 'HHTh4BmIT4',
+    password: 'zOl8IYGf1s',
 });
 
 // local database
 const poolLocal = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'comp4944-project',
-  password: 'COMP-4711',
+    host: 'localhost',
+    user: 'root',
+    database: 'comp4944-project',
+    password: 'COMP-4711',
 });
 
 // uses poolLocal if poolRemote is not available
