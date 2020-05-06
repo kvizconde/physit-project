024f1f84f7598fd12ac41d27afb2f513c72ef2d5
Daniel Chu <n4jn4d@gmail.com>
2020-05-06 16:59:51 -0700

    added models for home, exercise, and timeline views

--- models/homeData.js
+++ models/homeData.js
@@ -1,9 +1,23 @@
 const db = require('../util/database');
 
-function getAllPTs() {
-    return db.execute(`SELECT * FROM physiotherapist`);
-  }
+function postAppointmentDetail(appointmentID, bodyPart, symptom, diagnosis, recoveryDate) {
+  db.execute(`INSERT INTO appointmentdetail (appointmentID, bodyPart, symptom, diagnosis, recoveryDate) 
+                  VALUES (${appointmentID}, "${bodyPart}", "${symptom}", "${diagnosis}", "${recoveryDate}");`);
+}
+
+function getAllActiveInjuries(patientID) {
+  db.execute(`SELECT * FROM appointmentdetail
+              WHERE patientID = ${patientID} AND active = 1;`);
+}
+
+function postSetInjuryInactive(appointmentID, bodypart) {
+  db.execute(`UPDATE appointmentdetail
+                  SET active = 0
+                  WHERE (appointmentID = ${appointmentID} AND bodypart = "${bodypart}");`);
+}
 
 exports.module = {
-    getAllPTs,
+  postAppointmentDetail,
+  getAllActiveInjuries,
+  postSetInjuryInactive
 };
