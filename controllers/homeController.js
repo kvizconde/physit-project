3afa8a0eef1eee06f5ba7dcd56f959c5e3fa3d73
BeeWang015 <bkwang015@outlook.com>
2020-05-07 16:47:29 -0700

    made the card view work dynamically

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,7 @@ const loginController = require("../controllers/loginController");
 
 // Get home
 exports.getHome = (req, res) => {
+  console.log(req.body);
   res.render("home", {
     title: "The Future of Physio",
     homepageJSCSS: true,
