ced617f8da2409a626cc3f098bc41129e194f55d
Daniel Chu <n4jn4d@gmail.com>
2020-05-11 16:09:45 -0700

    Daniel (#72)
    * daniel test push
    
    * added remotemysql info into database.js
    
    * added loggedIn.hbs for log in testing
    
    * added test and log in authentication routes to loginRoute.js
    
    * added test and log in functions to loginController.js
    
    * added getAllPTs and logIn functions to loginData.js
    
    * updated log in form action and input name
    
    * changed input type from email to text for physio ID log in
    
    * merged benson changes to loginRoute and index
    
    * fixed code formatting in loginController
    
    * switched to remotemysql db functions
    
    * fixed formatting in loginRoute
    
    * switched to remotemysql db
    
    * fixed name variables to match db
    
    * pulls appointment from db, includes date
    
    * added function to pull appointments from db, includes date
    
    * updated log in route to include appointment pull
    
    * added dynamic data pulling from db
    
    * created controller and model for patientList page.
    
    * added date parameter into getPatientAppointmentData
    
    * added a back-up database from clever-cloud (commented out)
    
    * added models for home, exercise, and timeline views
    
    * created separate function to get current date
    
    * added authentication for user login
    
    * added patientListRoute and modified app start
    
    * created patientListRoute.js
    
    * added function to get physiotherapist
    
    * fixed log in controller to be dedicated to log in only
    
    * created patientListContoller to load patient list
    
    * modified log in function, made it async
    
    * added function to pull patients from db
    
    * removed old path, added new log in path
    
    * added express-session
    
    * made some temporary changes to hbs and css in patientView
    
    * fixed routes in log in and home views
    
    * fixed spacing in patientListController
    
    * added function that pulls patient name from db
    
    * renamed log in route
    
    * added appointmentID to data pulled
    
    * added function to render home view with patient name
    
    * fixed style.css and .home-container to patientList.css
    
    * added function to pull active injuries from db
    
    * saved patientID to req.session

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,8 +3,9 @@ const homeModel = require('../models/homeData')
 // Get home
 exports.getHome = async (req, res) => {
   try {
-    if(req.query.appointmentID) {
+    if(req.query.appointmentID && req.query.patientID) {
       req.session.appointmentID = req.query.appointmentID
+      req.session.patientID = req.query.patientID
     }
     const appID = req.session.appointmentID
     const patient = await homeModel.getAppointmentDetail(appID)
@@ -14,6 +15,7 @@ exports.getHome = async (req, res) => {
       homepageJSCSS: true,
       patient: patient[0][0],
     })
+    console.log(req.session.patientID)
   } catch (error) {
     throw error
   }
