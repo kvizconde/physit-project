7557f4cfbd0556915d872d6af27c707a79c0e811
Kevin Vizconde <kcarlv@gmail.com>
2020-05-12 03:03:07 -0700

    added back button

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,40 +1,48 @@
-const homeModel = require('../models/homeData')
+// const homeModel = require('../models/homeData')
 
 // Get home
-exports.getHome = async (req, res) => {
-  try {
-    if(req.query.appointmentID && req.query.patientID) {
-      req.session.appointmentID = req.query.appointmentID
-      req.session.patientID = req.query.patientID
-    }
-    const appID = req.session.appointmentID
-    const getPatient = await homeModel.getAppointmentDetail(appID)
-    const patient = parsePatientInfo(getPatient)
-    req.session.patientInfo = patient
+// exports.getHome = async (req, res) => {
+//   try {
+//     if(req.query.appointmentID && req.query.patientID) {
+//       req.session.appointmentID = req.query.appointmentID
+//       req.session.patientID = req.query.patientID
+//     }
+//     const appID = req.session.appointmentID
+//     const getPatient = await homeModel.getAppointmentDetail(appID)
+//     const patient = parsePatientInfo(getPatient)
+//     req.session.patientInfo = patient
 
-    res.render('home', {
-      title: 'Patient Home',
-      homepageJSCSS: true,
-      patient: patient,
-    })
-    console.log(req.session.patientID)
-  } catch (error) {
-    throw error
-  }
-}
+//     res.render('home', {
+//       title: 'Patient Home',
+//       homepageJSCSS: true,
+//       patient: patient,
+//     })
+//     console.log(req.session.patientID)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-const parsePatientInfo = (info) => {
-  try {
-    const data = info[0][0]
-    const patientInfo = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      dateOfBirth: data.dateOfBirth
-    }
-    return patientInfo
-  } catch (error) {
-    throw error
-  }
-  
-}
+// const parsePatientInfo = (info) => {
+//   try {
+//     const data = info[0][0]
+//     const patientInfo = {
+//       firstName: data.firstName,
+//       lastName: data.lastName,
+//       dateOfBirth: data.dateOfBirth
+//     }
+//     return patientInfo
+//   } catch (error) {
+//     throw error
+//   }
 
+// }
+
+// LOCAL - DON'T TOUCH
+// Get Home
+exports.getHome = (req, res) => {
+  res.render('home', {
+    title: 'Home',
+    homepageJSCSS: true,
+  });
+};
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const homeController = require("../controllers/homeController");
+const homeController = require('../controllers/homeController');
 const auth = require('../util/auth');
 
-router.route("/home").get(auth, homeController.getHome);
+// router.route("/home").get(auth, homeController.getHome);
+
+// LOCAL - DON'T TOUCH
+router.route('/home').get(homeController.getHome);
 
 module.exports = router;
