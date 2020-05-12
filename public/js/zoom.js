bc5db9f9259b6753775a27ecd33e95886f9c3308
Kevin Vizconde <kcarlv@gmail.com>
2020-05-11 17:46:06 -0700

    biodigital overhaul, made knee.hbs, updated shoulder view and 3d homepage

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -1,3 +1,24 @@
+// load shoulder symptoms/diagnosis and header
+$('#shoulder3d').on('load', () => {
+  $('#inputBox_shoulder').css({
+    transition: 'all .65s ease-out',
+    transform: 'translateX(0)',
+  });
+
+  $('#shoulderView').fadeIn(666);
+});
+
+// load knee symptoms/diagnosis and header
+$('#knee3d').on('load', () => {
+  $('#inputBox_knee').css({
+    transition: 'all .65s ease-out',
+    transform: 'translateX(0)',
+  });
+
+  $('#kneeView').fadeIn(666);
+});
+
+// style for input labels
 $('.form__symptoms')
   .focus(() => {
     $('.form__label.symptoms').attr('id', 'labelGlow');
--- routes/zoomRoute.js
+++ routes/zoomRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const zoomController = require('../controllers/zoomController');
 
 router.get('/shoulder', zoomController.getShoulder);
+router.get('/knee', zoomController.getKnee);
 
 module.exports = router;
