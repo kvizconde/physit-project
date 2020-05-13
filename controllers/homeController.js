f5a93c35a40c76782062b4f120d880e88a563f7e
Kevin Vizconde <kcarlv@gmail.com>
2020-05-12 19:27:15 -0700

    commented out local section for controllers/routes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,48 +1,49 @@
-// const homeModel = require('../models/homeData')
+const homeModel = require('../models/homeData');
 
 // Get home
-// exports.getHome = async (req, res) => {
-//   try {
-//     if(req.query.appointmentID && req.query.patientID) {
-//       req.session.appointmentID = req.query.appointmentID
-//       req.session.patientID = req.query.patientID
-//     }
-//     const appID = req.session.appointmentID
-//     const getPatient = await homeModel.getAppointmentDetail(appID)
-//     const patient = parsePatientInfo(getPatient)
-//     req.session.patientInfo = patient
+exports.getHome = async (req, res) => {
+  try {
+    if (req.query.appointmentID && req.query.patientID) {
+      req.session.appointmentID = req.query.appointmentID;
+      req.session.patientID = req.query.patientID;
+    }
+    const appID = req.session.appointmentID;
+    const getPatient = await homeModel.getAppointmentDetail(appID);
+    const patient = parsePatientInfo(getPatient);
 
-//     res.render('home', {
-//       title: 'Patient Home',
-//       homepageJSCSS: true,
-//       patient: patient,
-//     })
-//     console.log(req.session.patientID)
-//   } catch (error) {
-//     throw error
-//   }
-// }
+    req.session.patientInfo = patient;
 
-// const parsePatientInfo = (info) => {
-//   try {
-//     const data = info[0][0]
-//     const patientInfo = {
-//       firstName: data.firstName,
-//       lastName: data.lastName,
-//       dateOfBirth: data.dateOfBirth
-//     }
-//     return patientInfo
-//   } catch (error) {
-//     throw error
-//   }
+    res.render('home', {
+      title: 'Patient Home',
+      homepageJSCSS: true,
+      patient,
+    });
+    console.log(req.session.patientID);
+  } catch (error) {
+    throw error;
+  }
+};
 
-// }
+const parsePatientInfo = info => {
+  try {
+    const data = info[0][0];
+    const patientInfo = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      dateOfBirth: data.dateOfBirth,
+    };
+
+    return patientInfo;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // LOCAL - DON'T TOUCH
 // Get Home
-exports.getHome = (req, res) => {
-  res.render('home', {
-    title: 'Home',
-    homepageJSCSS: true,
-  });
-};
+// exports.getHome = (req, res) => {
+//   res.render('home', {
+//     title: 'Home',
+//     homepageJSCSS: true,
+//   });
+// };
