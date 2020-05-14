4b4d8ae6992b736842c34504b6d997cebb6bbed8
Kevin Vizconde <kcarlv@gmail.com>
2020-05-14 11:17:50 -0700

    updated patientList, converted back to floating cards as per rahuls feedback

--- routes/patientListRoute.js
+++ routes/patientListRoute.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const patientListController = require('../controllers/patientListController');
 const auth = require('../util/auth');
 
-router.route('/patientList').get(auth, patientListController.renderPatientList)
+// router.route('/patientList').get(auth, patientListController.renderPatientList)
+
+// LOCAL - DON'T TOUCH
+router.route('/patientList').get(patientListController.renderPatientList);
 
 module.exports = router;
