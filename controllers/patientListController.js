bee555ebf76119d350e655126794c711faa8f6cd
Kevin Vizconde <kcarlv@gmail.com>
2020-05-14 17:02:47 -0700

    revamp patientlist hbs, css, converted back to cards, updated naming convention and changed layout model to flexbox

--- controllers/patientListController.js
+++ controllers/patientListController.js
@@ -1,48 +1,48 @@
-// const patientListModel = require('../models/patientListData');
-// const loginModel = require('../models/loginData');
+const patientListModel = require('../models/patientListData');
+const loginModel = require('../models/loginData');
 
-// const getDate = () => {
-//   const d = new Date('April 30, 2020 01:15:00');
-//   const year = d.getFullYear().toString();
-//   const day = d.getDate();
-//   let month = d.getMonth();
+const getDate = () => {
+  const d = new Date('April 30, 2020 01:15:00');
+  const year = d.getFullYear().toString();
+  const day = d.getDate();
+  let month = d.getMonth();
 
-//   if (month < 9) {
-//     month += 1;
-//     month = `0${month}`;
-//   } else {
-//     month += 1;
-//     month.toString();
-//   }
+  if (month < 9) {
+    month += 1;
+    month = `0${month}`;
+  } else {
+    month += 1;
+    month.toString();
+  }
 
-//   const date = `${year}-${month}-${day}`;
+  const date = `${year}-${month}-${day}`;
 
-//   return date;
-// };
+  return date;
+};
 
-// exports.renderPatientList = async (req, res) => {
-//   try {
-//     const { physioID } = req.session;
-//     const physiotherapist = await loginModel.getPhysiotherapist(physioID);
-//     const patientList = await patientListModel.getPatientAppointmentData(physioID, getDate());
+exports.renderPatientList = async (req, res) => {
+  try {
+    const { physioID } = req.session;
+    const physiotherapist = await loginModel.getPhysiotherapist(physioID);
+    const patientList = await patientListModel.getPatientAppointmentData(physioID, getDate());
 
-//     res.render('patientList', {
-//       title: 'HOME',
-//       patientListJSCSS: true,
-//       indexJSCSS: false,
-//       physiotherapist: physiotherapist[0][0],
-//       patients: patientList[0],
-//     });
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+    res.render('patientList', {
+      title: 'HOME',
+      patientListJSCSS: true,
+      indexJSCSS: false,
+      physiotherapist: physiotherapist[0][0],
+      patients: patientList[0],
+    });
+  } catch (error) {
+    throw error;
+  }
+};
 
 // LOCAL - DON'T TOUCH
-exports.renderPatientList = (req, res) => {
-  res.render('patientList', {
-    title: 'HOME',
-    patientListJSCSS: true,
-    indexJSCSS: false,
-  });
-};
+// exports.renderPatientList = (req, res) => {
+//   res.render('patientList', {
+//     title: 'HOME',
+//     patientListJSCSS: true,
+//     indexJSCSS: false,
+//   });
+// };
