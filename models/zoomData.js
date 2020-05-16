5d1c9619763995c1d9810797efb85276c62ba493
Daniel Chu <n4jn4d@gmail.com>
2020-05-16 16:18:24 -0700

    Daniel (#87)
    * daniel test push
    
    * added remotemysql info into database.js
    
    * added loggedIn.hbs for log in testing
    
    * added test and log in authentication routes to loginRoute.js
    
    * added test and log in functions to loginController.js
    
    * added getAllPTs and logIn functions to loginData.js
    
    * updated log in form action and input name
    
    * changed input type from email to text for physio ID log in
    
    * merged benson changes to loginRoute and index
    
    * fixed code formatting in loginController
    
    * switched to remotemysql db functions
    
    * fixed formatting in loginRoute
    
    * switched to remotemysql db
    
    * fixed name variables to match db
    
    * pulls appointment from db, includes date
    
    * added function to pull appointments from db, includes date
    
    * updated log in route to include appointment pull
    
    * added dynamic data pulling from db
    
    * created controller and model for patientList page.
    
    * added date parameter into getPatientAppointmentData
    
    * added a back-up database from clever-cloud (commented out)
    
    * added models for home, exercise, and timeline views
    
    * created separate function to get current date
    
    * added authentication for user login
    
    * added patientListRoute and modified app start
    
    * created patientListRoute.js
    
    * added function to get physiotherapist
    
    * fixed log in controller to be dedicated to log in only
    
    * created patientListContoller to load patient list
    
    * modified log in function, made it async
    
    * added function to pull patients from db
    
    * removed old path, added new log in path
    
    * added express-session
    
    * made some temporary changes to hbs and css in patientView
    
    * fixed routes in log in and home views
    
    * fixed spacing in patientListController
    
    * added function that pulls patient name from db
    
    * renamed log in route
    
    * added appointmentID to data pulled
    
    * added function to render home view with patient name
    
    * fixed style.css and .home-container to patientList.css
    
    * added function to pull active injuries from db
    
    * saved patientID to req.session
    
    * patient name added to all views
    
    * added patient name to knee and shoulder zoom
    
    * patient age, weight, and height dynamically displayed
    
    * added time format to exclude seconds
    
    * pulled age, weight, and height from db
    
    * added age, height, weight to patientInfo
    
    * added model for zoom view db functions
    
    * added conditional code that loads existing db info into the symptoms and diagnosis fields
    
    * added route to save data into db
    
    * removed postAppointmentDetail function
    
    * added functions that saves symptoms/diagnosis into db, and loads zoomView with existing info
    
    * removed biodigital script
    
    * modified form to include delete and complete buttons
    
    * added routes for deleting and completing injuries
    
    * added function to delete and complete injuries in DB
    
    * added calendar for recovery date input
    
    * added dynamic recovery date input from user to be entered into DB

--- models/zoomData.js
+++ models/zoomData.js
@@ -1,19 +1,20 @@
 const db = require("../util/database");
 
-async function postAppointmentDetail(patientID, appointmentID, bodyPart, symptom, diagnosis, recoveryDate) {
+async function postAppointmentDetail(patientID, appointmentID, bodypart, symptom, diagnosis, recoveryDate) {
   await db.execute(
-    `INSERT INTO appointmentdetail (patientID, appointmentID, bodyPart, symptom, diagnosis, recoveryDate) 
-         VALUES (${patientID}, ${appointmentID}, "${bodyPart}", "${symptom}", "${diagnosis}", "${recoveryDate}");`
+    `INSERT INTO appointmentdetail (patientID, appointmentID, bodypart, symptom, diagnosis, recoveryDate) 
+         VALUES (${patientID}, ${appointmentID}, "${bodypart}", "${symptom}", "${diagnosis}", "${recoveryDate}");`
   )
 }
 
 async function getInjury(patientID, bodypart) {
   try {
     const response = await db.execute(
-      `SELECT *
+      `SELECT symptom, diagnosis, DATE_FORMAT(recoveryDate, "%Y-%m-%d") AS recoveryDate
        FROM appointmentdetail
        WHERE patientID = ${patientID}
-           AND bodypart = "${bodypart}";`
+           AND bodypart = "${bodypart}"
+           AND active = 1;`
     );
     
     if (response[0].length === 0) {
@@ -26,14 +27,45 @@ async function getInjury(patientID, bodypart) {
   }
 }
 
-async function updateInjury(patientID, bodypart, symptom, diagnosis) {
+async function updateInjury(patientID, bodypart, symptom, diagnosis, recoveryDate) {
   try {
     await db.execute(
       `UPDATE appointmentdetail
-       SET symptom = "${symptom}", diagnosis = "${diagnosis}"
+       SET symptom = "${symptom}", diagnosis = "${diagnosis}", recoveryDate = "${recoveryDate}"
        WHERE patientID = ${patientID}
-           AND bodypart = "${bodypart}";`
-    )
+           AND bodypart = "${bodypart}"
+           AND active = 1;`
+    );
+    return
+  } catch (error) {
+    throw error
+  }
+}
+
+async function deleteInjury(patientID, bodypart) {
+  try {
+    await db.execute(
+      `DELETE FROM appointmentdetail
+       WHERE patientID = ${patientID}
+           AND bodypart = "${bodypart}"
+           AND active = 1;`
+    );
+    return
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function completeInjury(patientID, bodypart) {
+  try {
+    await db.execute(
+      `UPDATE appointmentdetail
+       SET active = 0
+       WHERE patientID = ${patientID}
+           AND bodypart = "${bodypart}"
+           AND active = 1;`
+    );
+    return
   } catch (error) {
     throw error
   }
@@ -42,5 +74,7 @@ async function updateInjury(patientID, bodypart, symptom, diagnosis) {
 module.exports = {
   postAppointmentDetail,
   getInjury,
-  updateInjury
+  updateInjury,
+  deleteInjury,
+  completeInjury
 };
