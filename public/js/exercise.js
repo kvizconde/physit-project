b280db071fc1a2682a23905145806857f018fce5
Kevin Vizconde <kcarlv@gmail.com>
2020-05-17 01:17:37 -0700

    updated exercise css, hbs, and moved js script into exercise js file

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -1,1 +1,99 @@
-// TODO: IF NEEDED
+$('#preloader')
+  .css('visibility', 'visible')
+  .hide()
+  .fadeIn(666);
+
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+
+function ready() {
+  const removeExerciseItemButton = document.getElementsByClassName('btn-remove');
+
+  for (var i = 0; i < removeExerciseItemButton.length; i++) {
+    var button = removeExerciseItemButton[i];
+
+    button.addEventListener('click', removeExerciseItem);
+  }
+
+  const addToCartButtons = document.getElementsByClassName('add-exercise-btn');
+
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+
+    button.addEventListener('click', addExerciseClicked);
+  }
+
+  document.getElementsByClassName('btn-send')[0].addEventListener('click', sendToPatientClicked);
+}
+
+function sendToPatientClicked() {
+  alert('Sending to Patient');
+  const cartItems = document.getElementsByClassName('cart-items')[0];
+
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+}
+
+function removeExerciseItem(event) {
+  const buttonClicked = event.target;
+
+  buttonClicked.parentElement.parentElement.remove();
+}
+
+function quantityChanged(event) {
+  const input = event.target;
+
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+}
+
+function addExerciseClicked(event) {
+  const button = event.target;
+  const shopItem = button.parentElement.parentElement;
+  const title = shopItem.getElementsByClassName('single-exercise-title')[0].innerText;
+  const equipment = shopItem.getElementsByClassName('single-exercise-equipment')[0].innerText;
+  const imageSrc = shopItem.getElementsByClassName('single-exercise-image')[0].src;
+
+  addExerciseToCart(title, equipment, imageSrc);
+}
+
+function addExerciseToCart(title, equipment, imageSrc) {
+  const cartRow = document.createElement('div');
+
+  cartRow.classList.add('cart-row');
+  const cartItems = document.getElementsByClassName('cart-items')[0];
+  const cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+
+  for (let i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert('Exercise Already Added');
+      return;
+    }
+  }
+  const cartRowContents = `
+        <div class="cart-item cart-column">
+        <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-equipment cart-column">${equipment}</span>
+        <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="6">
+        </div>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+        </div>
+        <div class="cart-quantity cart-column">
+        <button class="btn btn-remove" type="button">REMOVE</button>
+    </div>`;
+
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeExerciseItem);
+  cartRow
+    .getElementsByClassName('cart-quantity-input')[0]
+    .addEventListener('change', quantityChanged);
+}
